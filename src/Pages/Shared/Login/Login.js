8cfa02dc8866493143faa62b8728b4dcc3fdfb2c
Mesuk23 <hadeulislammesuk23@gmail.com>
2022-10-25 01:54:57 +0600

    login and register form added

--- src/Pages/Shared/Login/Login.js
+++ src/Pages/Shared/Login/Login.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div>
-            This is login
+            <Form className=' w-50 mx-auto mt-5'>
+                <Form.Group className="mb-3">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control name="email" type="email" placeholder="Enter email" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control name="passwerd" type="password" placeholder="Password" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Text>
+                        New to this website ? Please <Link to="/register">Register</Link>
+                    </Form.Text>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Register
+                </Button>
+            </Form>
         </div>
     );
 };
--- src/Pages/Shared/Register/Register.js
+++ src/Pages/Shared/Register/Register.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
         <div>
-            This is register
+            <Form className=' w-50 mx-auto mt-5'>
+                <Form.Group className="mb-3">
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control name="name" type="text" placeholder="Enter your name" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control name="email" type="email" placeholder="Enter email" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control name="passwerd" type="password" placeholder="Password" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Text>
+                        Already have a account? please <Link to="/login">Log in</Link>
+                    </Form.Text>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Login
+                </Button>
+            </Form>
         </div>
     );
 };
