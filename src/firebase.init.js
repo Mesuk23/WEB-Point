72e363de9e4f40673b7da16161601d1c8fbc7e4c
Mesuk23 <hadeulislammesuk23@gmail.com>
2022-10-27 15:21:52 +0600

    environment variable problem solved

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,19 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-
-    apiKey: "AIzaSyAdWKJuJharIhj3GSRG1N8lBPjXAK1_YeE",
-    authDomain: "assignment-10-cdb34.firebaseapp.com",
-    projectId: "assignment-10-cdb34",
-    storageBucket: "assignment-10-cdb34.appspot.com",
-    messagingSenderId: "274137942750",
-    appId: "1:274137942750:web:238ee4cf37bb0fc02a504f"
-    // apiKey: process.env.REACT_APP_apiKey,
-    // authDomain: process.env.REACT_APP_authDomain,
-    // projectId: process.env.REACT_APP_projectId,
-    // storageBucket: process.env.REACT_APP_storageBucket,
-    // messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    // appId: process.env.REACT_APP_appId
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
