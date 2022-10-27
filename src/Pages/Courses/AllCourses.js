3e54d508904f530e56d3c7f443b6e6db75231681
Mesuk23 <hadeulislammesuk23@gmail.com>
2022-10-27 22:53:20 +0600

    pdf file added

--- src/Pages/Courses/AllCourses.js
+++ src/Pages/Courses/AllCourses.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import ReactDOM from "react-dom";
 import Pdf from "react-to-pdf";
 import { createRef } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const AllCourses = ({ course }) => {
     const ref = createRef();
@@ -14,16 +15,16 @@ const AllCourses = ({ course }) => {
     const { name, details, photoUrl, price, id } = course;
     console.log(course);
     return (
-        <div className='col-md-4'>
+        <div>
 
 
             <Card className='mb-3 w-80 mx-auto text-dark me-0'>
 
-                <Card.Img variant="top" src={photoUrl} />
+                <Card.Img variant="top" height={200} src={photoUrl} />
                 <Card.Body>
                     <Card.Title>{name}</Card.Title>
                     <Card.Text>
-                        {details.slice(0, 250)} ...<Link to={`course-details/${id}`}>read more</Link>
+                        {details.slice(0, 200)} ...<Link to={`course-details/${id}`}>read more</Link>
                     </Card.Text>
                     <Card.Text>
                         <h5> Price:{price}</h5>
@@ -35,26 +36,31 @@ const AllCourses = ({ course }) => {
                         <Link to={`checkout/${id}`}>
                             <Button variant="danger">Checkout</Button>
                         </Link>
+                        <a href='webDev.pdf' download="webDev.pdf">
+                            <FaDownload></FaDownload>
+                        </a>
 
                         {/* <div>
-                            <FaDownload>
-                                <div className="App">
-                                    <Pdf targetRef={ref} filename="code-example.pdf">
-                                        {({ toPdf }) => <button onClick={toPdf}>Generate Pdf</button>}
-                                    </Pdf>
-                                    <div ref={ref}>
-                                        <h1>Hello CodeSandbox</h1>
-                                        <h2>Start editing to see some magic happen!</h2>
-                                    </div>
-                                </div>
-                            </FaDownload>
-                        </div> */}
+            <FaDownload>
+                <div className="App">
+                    <Pdf targetRef={ref} filename="code-example.pdf">
+                        {({ toPdf }) => <button onClick={toPdf}>Generate Pdf</button>}
+                    </Pdf>
+                    <div ref={ref}>
+                        <h1>Hello CodeSandbox</h1>
+                        <h2>Start editing to see some magic happen!</h2>
+                    </div>
+                </div>
+            </FaDownload>
+        </div> */}
 
                     </div>
 
 
                 </Card.Body>
             </Card>
+
+
 
 
 
--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -1,30 +1,29 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
 import { Link, useLoaderData } from 'react-router-dom';
 import AllCourses from './AllCourses';
 
 const Courses = () => {
     const courses = useLoaderData();
     return (
-        <div className='mt-5'>
+        <div>
+            <h1 className='text-center text-secondary mt-3'>My Courses </h1> <hr />
+            <div className='mt-5 all-container container'>
 
 
-            <Container>
-                <Row>
-                    <Col className='border-end' lg='3'>
-                        <h2 className='mb-3'>Category</h2>
-                        {
-                            courses.map(course => <p> <Link to={`course-details/${course.id}`}> {course.name}</Link></p>)
-                        }
-                    </Col>
-                    <Col lg='9'>
-                        {
-                            courses.map(course => <AllCourses key={course.id} course={course}></AllCourses>)
-                        }
 
-                    </Col>
-                </Row>
-            </Container>
+                <div className='border-end ' lg='3'>
+                    <h2 className='mb-3'>Category</h2>
+                    {
+                        courses.map(course => <p> <Link to={`course-details/${course.id}`}> {course.name}</Link></p>)
+                    }
+                </div>
+                <div className='courses-container'>
+                    {
+                        courses.map(course => <AllCourses key={course.id} course={course}></AllCourses>)
+                    }
+                </div>
+
+            </div>
         </div>
     );
 };
